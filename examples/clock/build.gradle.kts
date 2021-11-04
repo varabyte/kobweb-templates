c6938ab3ddc47c05afdd8c1a1bb2ec244b218b76
David Herman <bitspittle@gmail.com>
2021-11-04 16:18:20 -0700

    Add new clock sample

---
+++ examples/clock/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kobweb.application)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+    maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+}
+
+group = "clock"
+version = "1.0-SNAPSHOT"
+
+kotlin {
+    js(IR) {
+        moduleName = "clock"
+        browser {
+            commonWebpackConfig {
+                outputFileName = "clock.js"
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(libs.kobweb.core)
+                implementation(libs.kobweb.silk.core)
+                implementation(libs.kobweb.silk.icons.fa)
+             }
+        }
+    }
+}
