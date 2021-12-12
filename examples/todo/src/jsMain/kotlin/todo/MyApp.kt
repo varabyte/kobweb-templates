7c8953fb6293e0c28ad26adc5506856a3d522868
David Herman <bitspittle@gmail.com>
2021-12-12 14:42:17 -0800

    Update todo app with new API changes from 0.7.6

--- examples/todo/src/jsMain/kotlin/todo/MyApp.kt
+++ examples/todo/src/jsMain/kotlin/todo/MyApp.kt
@@ -3,27 +3,31 @@ package todo
 import androidx.compose.runtime.*
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.styleModifier
 import com.varabyte.kobweb.core.App
+import com.varabyte.kobweb.silk.InitSilk
+import com.varabyte.kobweb.silk.InitSilkContext
 import com.varabyte.kobweb.silk.SilkApp
 import com.varabyte.kobweb.silk.components.layout.Surface
+import com.varabyte.kobweb.silk.theme.registerBaseStyle
 import org.jetbrains.compose.web.css.*
 
 const val BORDER_COLOR = "#eaeaea"
 
-object TodoStyleSheet : StyleSheet() {
-    init {
-        "body" style {
+@InitSilk
+fun initSiteStyles(ctx: InitSilkContext) {
+    ctx.config.registerBaseStyle("body") {
+        Modifier.styleModifier {
             fontFamily(
                 "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Oxygen", "Ubuntu",
                 "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "sans-serif"
             )
         }
+    }
 
-        "footer" style {
-            width(100.percent)
-            height(100.px)
+    ctx.config.registerBaseStyle("footer") {
+        Modifier.width(100.percent).height(100.px).fontSize(1.5.cssRem).styleModifier {
             property("border-top", "1px solid $BORDER_COLOR")
-            fontSize(1.5.cssRem)
 
             display(DisplayStyle.Flex)
             justifyContent(JustifyContent.Center)
@@ -35,7 +39,6 @@ object TodoStyleSheet : StyleSheet() {
 @App
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
-    Style(TodoStyleSheet)
     SilkApp {
         Surface(Modifier.width(100.vw).height(100.vh)) {
             content()
