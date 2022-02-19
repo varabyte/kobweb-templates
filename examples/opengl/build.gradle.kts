078e43dbc411e9562a0d1b62aa1c0b522d482236
David Herman <bitspittle@gmail.com>
2022-02-19 14:40:10 -0800

    New OpenGL sample

---
+++ examples/opengl/build.gradle.kts
@@ -0,0 +1,45 @@
+import com.varabyte.kobweb.gradle.application.extensions.index
+import kotlinx.html.script
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kobweb.application)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+    maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
+}
+
+group = "opengl"
+version = "1.0-SNAPSHOT"
+
+kobweb.index.head.add {
+    script {
+        src = "https://cdnjs.cloudflare.com/ajax/libs/gl-matrix/3.4.2/gl-matrix-min.js"
+    }
+}
+
+kotlin {
+    js(IR) {
+        moduleName = "opengl"
+        browser {
+            commonWebpackConfig {
+                outputFileName = "opengl.js"
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(libs.kobweb.core)
+                implementation(libs.kobweb.silk.core)
+            }
+        }
+    }
+}
