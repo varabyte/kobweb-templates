d39ce9765db1fd1f1351b09c84719a10e253829e
David Herman <bitspittle@gmail.com>
2024-03-07 16:11:40 -0800

    Update Kobweb to v0.17.1

--- examples/imageprocessor/worker/src/jsMain/kotlin/imageprocessor/worker/ImageProcessorWorkerFactory.kt
+++ examples/imageprocessor/worker/src/jsMain/kotlin/imageprocessor/worker/ImageProcessorWorkerFactory.kt
@@ -1,53 +1,23 @@
-@file:UseSerializers(ImageDataSerializer::class)
 @file:Suppress("UnsafeCastFromDynamic") // Necessary for working with Uint8ClampedArray, otherwise values get truncated
 
 package imageprocessor.worker
 
+import com.varabyte.kobweb.worker.OutputDispatcher
+import com.varabyte.kobweb.worker.Transferables
 import com.varabyte.kobweb.worker.WorkerFactory
 import com.varabyte.kobweb.worker.WorkerStrategy
 import com.varabyte.kobwebx.worker.kotlinx.serialization.util.createIOSerializer
 import imageprocessor.util.ImageProcessor
+import imageprocessor.util.Image
 import imageprocessor.util.MutableImage
 import imageprocessor.util.processors.*
 import imageprocessor.util.toUint8ClampedArray
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.UseSerializers
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.Json
-import org.khronos.webgl.get
-import org.khronos.webgl.set
 import org.w3c.dom.ImageData
 
 @Serializable
-@SerialName("ImageData")
-private class ImageDataSurrogate(val w: Int, val h: Int, val data: IntArray)
-
-private object ImageDataSerializer : KSerializer<ImageData> {
-    private val surrogateSerializer = ImageDataSurrogate.serializer()
-    override val descriptor = surrogateSerializer.descriptor
-
-    override fun serialize(encoder: Encoder, value: ImageData) {
-        val surrogate =
-            ImageDataSurrogate(value.width, value.height, IntArray(value.data.length) { i -> value.data[i].toInt() })
-
-        encoder.encodeSerializableValue(surrogateSerializer, surrogate)
-    }
-
-    override fun deserialize(decoder: Decoder): ImageData {
-        val surrogate = decoder.decodeSerializableValue(surrogateSerializer)
-
-        return ImageData(surrogate.w, surrogate.h).apply {
-            surrogate.data.forEachIndexed { index, byte -> data[index] = byte.asDynamic() }
-        }
-    }
-}
-
-@Serializable
 class ProcessImageRequest(
-    val imageData: ImageData,
     val commands: List<ImageProcessorCommand>
 )
 
@@ -67,19 +37,22 @@ sealed class ImageProcessorCommand {
     class Flip(val x: Boolean = false, val y: Boolean = false) : ImageProcessorCommand()
 }
 
+@Suppress("CanSealedSubClassBeObject") // Using classes as we may add parameters in the future
 @Serializable
 sealed class ProcessImageResponse {
     @Serializable
-    class Finished(val imageData: ImageData) : ProcessImageResponse()
+    class Finished : ProcessImageResponse()
 
     @Serializable
     class Error(val message: String) : ProcessImageResponse()
 }
 
+private fun Image.toImageData() = ImageData(this.toUint8ClampedArray(), w, h)
+
 internal class ImageProcessorWorkerFactory : WorkerFactory<ProcessImageRequest, ProcessImageResponse> {
     override fun createIOSerializer() = Json.createIOSerializer<ProcessImageRequest, ProcessImageResponse>()
 
-    override fun createStrategy(postOutput: (output: ProcessImageResponse) -> Unit) =
+    override fun createStrategy(postOutput: OutputDispatcher<ProcessImageResponse>)=
         WorkerStrategy<ProcessImageRequest> { input ->
             val processors = mutableListOf<ImageProcessor>()
             for (command in input.commands) {
@@ -95,16 +68,16 @@ internal class ImageProcessorWorkerFactory : WorkerFactory<ProcessImageRequest, 
             }
 
             try {
-                val image = MutableImage(input.imageData.width, input.imageData.height, input.imageData.data)
+                val imageData = transferables.getImageData("imageData")!!
+                val image = MutableImage(imageData.width, imageData.height, imageData.data)
 
                 processors.forEach { it.process(image) }
                 postOutput(
-                    ProcessImageResponse.Finished(
-                        ImageData(image.toUint8ClampedArray(), image.w, image.h)
-                    )
+                    ProcessImageResponse.Finished(),
+                    Transferables { add("imageData", image.toImageData()) }
                 )
             } catch (e: Exception) {
-                postOutput(ProcessImageResponse.Error(e.message ?: "Unknown error"))
+                postOutput(ProcessImageResponse.Error(e.message ?: "Unknown error (${e::class.simpleName})"))
             }
         }
 }
