5deafcdcdeb047c3d29918a9f63db59b4d82da5c
David Herman <bitspittle@gmail.com>
2024-10-10 12:27:29 -0700

    Remove obsolete plugin repository
    We don't need it after K2, as the Compose plugin is now embedded
    in the Kotlin Gradle plugin.

--- app/empty/build.gradle.kts
+++ app/empty/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 subprojects {
     repositories {
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         maven("https://us-central1-maven.pkg.dev/varabyte-repos/public")
     }
